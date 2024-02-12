e1a15605492db7a0a6c0e1820926a85705e7a478
kadirl <kadir20016@gmail.com>
2024-02-13 00:12:18 +0600

    completed. adaptivity on the way

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// motion switch thing (unfunctional)
+
 // Get the checkbox element
 const motionSwitch = document.getElementById('motionSwitch');
 
@@ -17,3 +19,29 @@ motionSwitch.addEventListener('change', function() {
   }
 
 });
+
+
+// adaptive scaledown
+
+let originalHeight = document.querySelector('.posts').offsetHeight;
+
+function resize() {
+  let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var originalWidth = 1360 + 32 + 32;
+  // window width / original width = factor
+  var scalingFactor = windowWidth / originalWidth;
+
+  if (windowWidth < originalWidth && windowWidth > 960) {
+    document.querySelector('.posts').style.transform = 'scale(' + scalingFactor + ')';
+    document.querySelector('.posts').style.minHeight = (originalHeight * scalingFactor) + 'px';
+    document.querySelector('.posts').style.maxHeight = (originalHeight * scalingFactor) + 'px';
+    console.log(originalWidth + ", " + windowWidth + ", " + scalingFactor);
+  } else if (windowWidth <= 960) {
+    document.querySelector('.posts').removeAttribute("style");
+  }
+}
+
+window.addEventListener('resize', resize);
+document.addEventListener('DOMContentLoaded', resize);
+
+
